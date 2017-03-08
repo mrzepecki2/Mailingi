d0ec0fde089c6d91ec3c79e4a00a083d4aaa5243
mrzepecki2 <mateusz0345@gmail.com>
2017-03-08 14:30:44 +0100

    Mailingi

---
+++ 1/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta content="text/html; charset=ISO-8859-2" http-equiv="Content-Type" />
+	<title>enel-med | Centrum medyczne</title>
+</head>
+<body bgcolor="#ffffff" leftpadding="0" offset="0" paddingheight="0" paddingwidth="0" style="paddig: 0; margin: 0; width: 100%;" toppadding="0">
+	<table bgcolor="#ffffff" width="600" border="0" cellpadding="0" cellspacing="0" style='margin: 0 auto; font-family: Calibri, Tahoma, sans-serif;'>
+	  <tr>
+	    <th rowspan="2" width='37' height='192' style='padding:0; margin:0;'></th>
+	    <th width='145' height='82'  style='padding:0; margin:0;'>
+		
+	    	<img src='cid:img_02.jpg' width='145' height='82' style='border: 0; display: block;' alt='Enel-med'/>
+	    </th>
+	    <th width='418' height='82'  style='padding:0; margin:0;'>
+			<p style="padding: 0; margin-right:  40px; font-size: 22px; color:#314f47; text-align: right; line-height:1.4">www.enel.pl</p>
+		</th>
+	  </tr>
+	  <tr>
+	    <td colspan="2" width='563' height='110'  style='padding:0; margin:0;'>
+			<p style="padding: 0; margin:  0; font-size: 53px; color:#314f47; text-align: left; line-height:1.4">
+			Nie płać za każdą wizytę
+			</p>
+		</td>	
+	  </tr>
+	</table>
+	
+	<table bgcolor="#ffffff" width="600" border="0" cellpadding="0" cellspacing="0" style='margin: 0 auto; font-family: Calibri, Tahoma, sans-serif;'>
+		<tr>
+			<th colspan="2" width='395' height='16'  style='padding:0; margin:0;'></th>
+			<th rowspan="3" width='150' height='151'  style='padding:0; margin:0;'>
+		
+				<img src='cid:img_06.png' width='152' height='150' style='border: 0; display: block;' alt='Enel-med'/>
+				
+			</th>
+			<th width='56' height='16'  style='padding:0; margin:0;'></th>
+		</tr>
+		<tr>
+			<td width='37' height='120'  style='padding:0; margin:0; background-color:#e40000'></td>
+			<td width='358' height='120'  style='padding:0; margin:0; background-color:#e40000'>
+				<a href="<KLIK>https://enel.pl/nie-lecz-sie-sam/</KLIK>" target="_blank">
+				<img src='cid:img_09.png' width='358' height='120' style='border: 0; display: block;' alt='Enel-med'/>
+				</a>
+			</td>
+			<td width='56' height='119'  style='padding:0; margin: 0 0 0 -1px; background-color:#e40000'></td>
+		</tr>
+		<tr>
+			<td colspan="2" width='395' height='14'  style='padding:0; margin:0;'></td>
+			<td width='56' height='14'  style='padding:0; margin:0;'></td>
+		</tr>
+	</table>
+
+	
+	<table bgcolor="#ffffff" width="600" border="0" cellpadding="0" cellspacing="0" style='margin: 0 auto; font-family: Calibri, Tahoma, sans-serif;'>
+		<tr>
+			
+			<td width='212' height='428'  style='padding:0; margin:0; background-color:#e40000'>
+			<a href ="<KLIK>https://enel.pl/ubezpieczenia/prywatna-opieka-medyczna-dla-ciebie/</KLIK>" target="_blank">
+				<img src='cid:imgg_02.jpg' width='212' height='428' style='border: 0; display: block;' alt='Enel-med'/>
+			</a>
+			</td>
+			
+			
+			<td width='176' height='428'  style='padding:0; margin:0; background-color:#e40000'>
+			<a href ="<KLIK>https://enel.pl/ubezpieczenia/opieka-dla-rodziny/</KLIK>" target="_blank">
+				<img src='cid:imgg_03.jpg' width='176' height='428' style='border: 0; display: block;' alt='Enel-med'/>
+			</a>
+			</td>
+			
+			
+			<td width='212' height='428'  style='padding:0; margin:0; background-color:#e40000'>
+			<a href ="<KLIK>https://enel.pl/ubezpieczenia/opieka-dla-seniora/</KLIK>" target="_blank">
+				<img src='cid:imgg_04.jpg' width='212' height='428' style='border: 0; display: block;' alt='Enel-med'/>
+			</a>
+			</td>
+		</tr>
+	
+	</table>
+	
+		<table bgcolor="#ffffff" width="600" border="0" cellpadding="0" cellspacing="0" style='margin: 0 auto; font-family: Calibri, Tahoma, sans-serif;'>
+		<tr>
+			<td width='600' height='85'  style='padding:0; margin:0; background-color:white'>
+				<p style="padding: 0; font-size: 33px; color:#314f47; text-align: center; line-height:0"><strong>Dowiedz się więcej</strong></p>
+				<p style="padding: 0; font-size: 22px; color:#314f47; text-align: center; line-height:0.3; font-weight: 300"> Wybierz pakiet medyczny dla Ciebie i Twojej rodziny.</p>
+			</td>
+		</tr>
+		<tr>
+		<tr>
+			<td width='600' height='30'  style='padding:0; margin:0; background-color:#109f7b'>
+		
+			</td>
+		</tr>
+	
+	</table>
+
+</body>
+</html>
